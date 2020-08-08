f492f63de4bca2dd8e4a18b1109c375a8a9554ac
eddieware <47250727+eddieware@users.noreply.github.com>
2020-08-08 06:53:48 -0500

    add_token

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_push_notifications/src/pages/home_page.dart';
 import 'package:flutter_push_notifications/src/pages/mensaje_page.dart';
+import 'package:flutter_push_notifications/src/provider/push_notification.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> navigatorKey =
+      new GlobalKey<NavigatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    final pushProvider = new PushNotificationsProvider();
+    pushProvider.initNotifications();
+
+    pushProvider.mensajesStream.listen((data) {
+      // print('argumento desde main: $argumento');
+      // Navigator.pushNamed(context, 'mensaje');
+      navigatorKey.currentState.pushNamed('mensaje', arguments: data);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //avigatorKey: navigatorKey,
+      navigatorKey: navigatorKey,
       title: 'Material App',
       initialRoute: 'home',
       routes: {
